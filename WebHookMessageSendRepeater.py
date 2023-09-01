598889280b0fc5c42461cd9da96b19534701d21e
EddyTheAnimator1 <92644021+EddyTheAnimator1@users.noreply.github.com>
2023-09-01 17:14:35 +0200

    Update WebHookMessageSendRepeater.py

--- WebHookMessageSendRepeater.py
+++ WebHookMessageSendRepeater.py
@@ -1,6 +1,6 @@
 # Remove the # on Time and interval_seconds if you want to wait a little before sending another message
 
-# Also The reason for inport random is because of raid protection. So lets say your message is 'Hi' then it will add 'Hi (RandomNumbersAndStringsHere)'
+# Also the reason for inport random is because of raid protection. So lets say your message is 'Hi' then it will add 'Hi (RandomNumbersAndStringsHere)'
 
 import requests
 import random
