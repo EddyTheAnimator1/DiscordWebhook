263e25e90cc7ba8538057d8186100579b4bc47af
EddyTheAnimator1 <92644021+EddyTheAnimator1@users.noreply.github.com>
2023-09-01 17:13:28 +0200

    Update WebHookMessageSendRepeater.py

--- WebHookMessageSendRepeater.py
+++ WebHookMessageSendRepeater.py
@@ -1,12 +1,12 @@
+# Remove the # on Time and interval_seconds if you want to wait a little before sending another message
+
 import requests
 import random
 import string
+#import time
 
 def send_randomized_message(webhook_url, content):
-    # Generate a random string of letters and numbers
     random_chars = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
-    
-    # Add the random string to the content
     randomized_content = f"{content} {random_chars}"
     
     data = {
@@ -15,7 +15,7 @@ def send_randomized_message(webhook_url, content):
     response = requests.post(webhook_url, json=data)
     
     if response.status_code == 204:
-        print("Message sent successfully.")
+        print(f"Message sent successfully: {randomized_content}")
     else:
         print(f"Failed to send message. Status code: {response.status_code}")
         print(response.text)
@@ -23,4 +23,8 @@ def send_randomized_message(webhook_url, content):
 if __name__ == "__main__":
     webhook_url = "YOUR_WEBHOOK_URL_HERE"
     message_content = "Hello from the webhook!"
-    send_randomized_message(webhook_url, message_content)
+    #interval_seconds = 5  # Adjust the interval as needed (e.g., 5 seconds)
+    
+    while True:
+        send_randomized_message(webhook_url, message_content)
+        time.sleep(interval_seconds)
